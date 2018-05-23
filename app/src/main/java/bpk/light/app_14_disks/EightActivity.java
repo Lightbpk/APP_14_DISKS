411256f86fa11480d7fb51312458632a3932a15e
Lightbpk <ebKeir-+>
2018-05-23 11:07:47 +0700

    swipe work search disk work slider and save work

--- app/src/main/java/bpk/light/app_14_disks/EightActivity.java
+++ app/src/main/java/bpk/light/app_14_disks/EightActivity.java
@@ -1,13 +1,28 @@
 package bpk.light.app_14_disks;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class EightActivity extends AppCompatActivity {
-
+ImageView carPhoto;
+Bitmap btm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eight);
+        carPhoto = findViewById(R.id.CarPhoto);
+        try{
+            btm = BitmapFactory.decodeFile("/mnt/sdcard/pictures/CarPhoto.jpg");
+            carPhoto.setImageBitmap(btm);
+            carPhoto.invalidate();
+        }catch (Exception e){
+            Log.d("LightLog", "Err decodin");
+        }
     }
 }
--- app/src/main/java/bpk/light/app_14_disks/FourActivity.java
+++ app/src/main/java/bpk/light/app_14_disks/FourActivity.java
@@ -1,13 +1,47 @@
 package bpk.light.app_14_disks;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
 
 public class FourActivity extends AppCompatActivity {
-
+Button btn_selectImg, btn_camera;
+String TAG = "LightLog";
+File file = new File("/mnt/sdcard/pictures/CarPhoto.jpg");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_four);
+        btn_selectImg = findViewById(R.id.btn_selectImg);
+        btn_camera = findViewById(R.id.btn_camera);
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                startActivityForResult(intent, 1);
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent intent) {
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                            Intent intent1 = new Intent(FourActivity.this, EightActivity.class);
+                            startActivity(intent1);
+            } else if (resultCode == RESULT_CANCELED) {
+                Log.d(TAG, "Canceled");
+            }
+        }
     }
 }
